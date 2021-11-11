3d3ed74dc89dd0eaad920980ddc568afe7f6effb
Igor Gonçalves <igordossantos@gmail.com>
2021-11-11 18:10:56 -0300

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 function protegercodigo() {
     if (event.button==2||event.button==3){
-        alert('Codigo protegido!');}
+        alert('Codigo protegido!');
+        }
     }
     document.onmousedown=protegercodigo
+
+function limparTextArea() {
+    const textarea = document.getElementById('msg');
+    // ... 
+}
