c543b2e27f607632886fa837ced7343d9178e686
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-22 17:31:15 -0300

    refactor(oldsite): move old repository to new
    Moving old respository to the new as a reference with usable files

---
+++ old_site/assets/js/script.js
@@ -0,0 +1,11 @@
+function protegercodigo() {
+    if (event.button==2||event.button==3){
+        alert('Codigo protegido!');
+        }
+    }
+    document.onmousedown=protegercodigo
+
+function limparTextArea() {
+    const textarea = document.getElementById('msg');
+    // ... 
+}
