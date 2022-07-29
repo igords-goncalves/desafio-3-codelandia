6f1a60ca85ffbefc13ba3bd5450b0264faf91c6e
Igor Gonçalves <igordossantos38@gmail.com>
2022-07-29 10:11:32 -0300

    refact(footer) create footer and form component
    Creating footer and form component taking advantage of button component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./form/Form";
+
+const Footer = props => {
+    return (
+        <div className="form">
+            <Form />
+        </div>
+    )
+}
+
+export default Footer;
