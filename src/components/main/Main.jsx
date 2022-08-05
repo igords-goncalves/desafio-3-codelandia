90c2412208ea7929e6e2201428239fc53bd129db
Igor Gonçalves <igordossantos38@gmail.com>
2022-08-05 15:45:35 -0300

    react(main) give style to main component
    Giving style to main component and import css file

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Cards from "./cards/Cards";
+import "./Main.css";
 
 const Main = (_) => {
     return (
         <main>
             <div className="cards">
                 <Cards
-                    className="cards-descript-1"
+                    className="cards-descript-1 box"
                     classColor="color-red"
+                    classDescript="descript"
+                    title="Testanto Componente"
+                    description="Elemento testado"
+                />
+                <Cards
+                    className="cards-descript-2 box"
+                    classColor="color-green"
                     classDescript="descript"
                     title="Testanto Componente"
                     description="Elemento testado"
