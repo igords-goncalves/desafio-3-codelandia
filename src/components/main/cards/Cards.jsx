36abbbcd1126a33214d74ee174ec01df2bdea3f6
Igor Gonçalves <igordossantos38@gmail.com>
2022-08-05 15:29:46 -0300

    refact(cards) create card elements
    Creating cards elements and className about node elements

---
+++ src/components/main/cards/Cards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cards = (props) => {
+    return (
+        <div className={props.className}>
+            <div className={props.classColor}></div>
+            <div className={props.classDescript}>
+                <h2>{props.title}</h2>
+                <p>{props.description}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
